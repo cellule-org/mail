26ca815f4d65fb6a403b3a26768cbd4dd24a3050
Impre-visible <romeo@chevrier.dev>
2025-03-11 21:51:50 +0100

    ✨ feat: Add environment type definitions and update package dependencies for email handling

---
+++ backend/src/env.d.ts
@@ -0,0 +1,14 @@
+declare namespace NodeJS {
+    interface ProcessEnv {
+        SMTP_HOST: string;
+        SMTP_PORT: string;  // Assurez-vous que les variables sont de type string car process.env retourne toujours des strings
+        SMTP_SECURE: boolean;
+        SMTP_USER: string;
+        SMTP_PASS: string;
+        IMAP_HOST: string;
+        IMAP_PORT: string;
+        IMAP_SECURE: boolean;
+        IMAP_USER: string;
+        IMAP_PASS: string;
+    }
+}
