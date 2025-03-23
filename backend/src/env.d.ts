8110712a65f1883d5cdcc70dda28bf6bff1b3576
Impre-visible <romeo@chevrier.dev>
2025-03-23 11:42:58 +0100

    ✨ feat: Remove SMTP and IMAP environment variables from env.d.ts and update tsconfig.json to include custom type definitions

--- backend/src/env.d.ts
+++ backend/src/env.d.ts
@@ -1,20 +1,4 @@
 declare namespace NodeJS {
     interface ProcessEnv {
-        SMTP_HOST: string;
-        SMTP_PORT: string;  // Assurez-vous que les variables sont de type string car process.env retourne toujours des strings
-        SMTP_SECURE: boolean;
-        SMTP_USER: string;
-        SMTP_PASS: string;
-        IMAP_HOST: string;
-        IMAP_PORT: string;
-        IMAP_SECURE: boolean;
-        IMAP_USER: string;
-        IMAP_PASS: string;
-
-        INBOX: string;
-        SENT: string;
-        DRAFTS: string;
-        TRASH: string;
-        SPAM: string;
     }
 }
